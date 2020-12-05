169d620b6056e8503ad6f217b34088c358d11d1a
yashi12 <yashiagarwal1812@gmail.com>
2020-12-05 07:34:38 +0530

    Change css

--- static/javascript/google-signin.js
+++ static/javascript/google-signin.js
@@ -3,27 +3,53 @@ googleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
         .then(function (result) {
             // This gives you a Google Access Token. You can use it to access the Google API.
-       // const Http = new XMLHttpRequest();
+            const Http = new XMLHttpRequest();
             const token = result.credential.accessToken;
+            console.log("token 1", token)
             // The signed-in user info.
             const user = result.user;
-            // fetch('/google',{
-            //     method:'POST',
-            //     redirect:'/google'
-            // })
+//             fetch('/google',{
+//                 method:'POST',
+//                 redirect:'/google'
+//             })
             //     .then(res=>{
             //         console.log("send",res);
             //     })
             //     .catch(err=>{
             //         console.log(err);
             //     })
-//       Http.open('POST','http://localhost:5000/google');
-//        Http.send();
-     console.log("user",user);
-     // sessionStorage.setItem('idToken',token)
+            console.log("user", user);
+//            $.ajax({
+//                type:'POST',
+//                contentType:'application/json;charset-utf-08',
+//                dataType:'json',
+//                url:'http://localhost:5000/google?value='+token,
+//                success:function (data) {
+//                    var reply=data.reply;
+//                    if (reply=="success")
+//                    {
+//                        location.href = '/';
+//                        console.log("user inner", user);
+//                        return;
+//                    }
+//                    else
+//                    {
+//                        alert("some error ocured in session agent")
+//                    }
+//
+//                }
+//            })
+       Http.open('POST','http://localhost:5000/google');
+       console.log("token",token)
+        resp = Http.send(token);
+
+            location.href = '/';
+            console.log("user", user);
+
+            // sessionStorage.setItem('idToken',token)
             // session['idToken'] = token;
 //            console.log("session",session['idToken']);
-        location.href = '/';
+//        location.href = '/';
 
             console.log("hello");
         })
@@ -52,7 +78,7 @@ verifyPassword = () => {
 }
 
 
-searchGivenSkill = (title)=>{
+searchGivenSkill = (title) => {
     document.getElementById("query").value = title;
     document.getElementById("searchQuery").click();
 }
