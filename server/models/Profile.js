724f08351d9f3adc5909af4a2dd8e2f143c4c336
yashi12 <yashiagarwal1812@gmail.com>
2021-03-10 22:03:15 +0530

    Initial backend code
    - User login,registration
    - Create,Update,Delete,Fetch User,user profile

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -33,7 +33,35 @@ const ProfileSchema = new mongoose.Schema({
     date:{
         type:Date,
         default:Date.now
-    }
+    },
+    experience:[
+        {
+            title:{
+                type:String,
+                required: true,
+                maxlength: 50
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            from:{
+                type:Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String,
+                maxlength: 150
+            }
+        }
+    ]
 });
 
 module.exports = mongoose.model('Profile',ProfileSchema);
