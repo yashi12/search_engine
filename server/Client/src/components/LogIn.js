80b3db802c5faa01187883e69f2c41cd1512d63a
21Siddhantjain <2001siddhantjain@gmail.com>
2021-03-27 17:48:52 +0530

    login and register update

--- server/Client/src/components/LogIn.js
+++ server/Client/src/components/LogIn.js
@@ -1,21 +1,48 @@
-const LogIn = () => {
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Link, Redirect } from 'react-router-dom'
+import { login } from '../action/auth'
+import  { useState } from 'react'
+
+const LogIn = ({ login, isAuthenticated }) => {
+
+    const [formData, setData] = useState({
+        email : '',
+        password : ''
+    })
+
+    const { email , password} = formData
+
+    const onChange = e => setData({...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        login({email, password})
+    }
+
+    // Redirect if logged in
+
+    if(isAuthenticated){
+        return <Redirect to='/profile' />
+    }
+
     return (
         <div className="row">
             <div className="col"></div>
             <div className="col">
             <br/><br/>
             <section className="search-bar">
-                <form className="bar" role="form" method="post" action="/api/token">
+                <form className="bar" role="form" method="post" action="/api/token" onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                         <h1>Log In</h1><br/>
                         <label for="exampleInputEmail1">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required=""
-                            name="email"></input>
+                            name="email" onChange={e => onChange(e)}></input>
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" name="password"></input>
+                        <input onChange={e => onChange(e)} type="password" className="form-control" id="exampleInputPassword1" name="password"></input>
                     </div>
                     <button type="submit" className="btn btn-primary">Login</button>
                     <div className="form-group text-center">
@@ -31,4 +58,13 @@ const LogIn = () => {
     )
 }
 
-export default LogIn
+LogIn.prototype = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, {login})(LogIn)
--- server/Client/src/reducers/auth.js
+++ server/Client/src/reducers/auth.js
@@ -1,6 +1,10 @@
 import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../action/types'
 
 const initialState = {
@@ -13,12 +17,22 @@ const initialState = {
 export default function(state = initialState, action){
     const { type, payload } = action
     switch(type){
+        case USER_LOADED :
+            return {
+                ...state,
+                isAuthenticated : true,
+                loading: false,
+                user: payload
+            }
         case REGISTER_SUCCESS :
+        case LOGIN_SUCCESS :
             localStorage.setItem('token',payload.token);
             return {
                 ...state, ...payload, isAuthenticated: true, loading: false
             }
         case REGISTER_FAIL :
+        case AUTH_ERROR :
+        case LOGIN_FAIL :
             localStorage.removeItem('token')
             return {
                 ...state, token: null, isAuthenticated: false, loading:false
