fc8bbd2861928d73ee512862ea3b8a2443821608
21Siddhantjain <2001siddhantjain@gmail.com>
2021-04-02 02:03:23 +0530

    profile add

--- server/Client/src/action/auth.js
+++ server/Client/src/action/auth.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { setAlert } from './alert'
 import {
     REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED,
-    AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT
+    AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT,
+    CLEAR_PROFILE
 } from './types'
 import setAuthToken from '../utils/setAuthToken'
 
@@ -104,6 +105,9 @@ export const login = ({ email, password}) => async dispatch => {
 // Log out
 
 export const logout = () => dispatch => {
+    dispatch({
+        type: CLEAR_PROFILE
+    })
     dispatch({
         type: LOGOUT
     })
