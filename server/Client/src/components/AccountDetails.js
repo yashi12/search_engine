4f5032e28c139716f81792f646df1cd62b0bb03e
yashi12 <yashiagarwal1812@gmail.com>
2021-04-21 17:29:30 +0530

    Connect frontended + backend:
    - add, view, like post
    - add profile

--- server/Client/src/components/AccountDetails.js
+++ server/Client/src/components/AccountDetails.js
@@ -5,25 +5,23 @@ import { createProfile } from '../action/profile'
 import { Link, withRouter } from 'react-router-dom'
 
 const AccountDetails = ({ createProfile, history }) => {
-
+    console.log("start return");
     const [formData, setFormData] = useState({
-        Github: '',
-        Linked_In:'',
+        githubusername: '',
+        linkedIn:'',
         twitter:'',
         bio:'',
-        skills:'',
-        experience:''
+        skills:''
     })
 
     const [displaySocialInputs, toggleSocialInputs] = useState(false)
 
-    const onChange = e => setFormData(...formData, [e.target.id]= e.target.value)
-
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
     const onSubmit = e => {
         e.preventDefault()
+        console.log("formgdata",formData)
         createProfile(formData, history)
     }
-
     return (
         <div>
             <div class="row">
@@ -32,44 +30,41 @@ const AccountDetails = ({ createProfile, history }) => {
                     <form onSubmit={e => onSubmit(e)}>
                         <br/><br/>
                         <div class="row">
-                            <button onClick={toggleSocialInputs(!displaySocialInputs)}>Add Social</button>
-                            { displaySocialInputs && <div>
+                            <button type="button" onClick={()=>toggleSocialInputs(!displaySocialInputs)}>Add Social</button>
+                            { displaySocialInputs &&( <div>
                                 <div class="col">
                                     <label>Github</label>
-                                    <input type="text" class="form-control" name="github" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                                    <input type="text" class="form-control" name="githubusername" aria-describedby="emailHelp" placeholder="Enter email" onChange={e => onChange(e)}></input>
                                 </div>
                                 <div class="col">
                                     <label>Linked In</label>
-                                    <input type="text" class="form-control" name="linked_in" placeholder="Password"></input>
+                                    <input type="text" class="form-control" name="linkedIn" placeholder="Password" onChange={e => onChange(e)}></input>
                                 </div>
-                                
+
                                 <div class="form-group">
                                     <label>Twitter</label>
-                                    <input type="text" class="form-control" name="twitter" placeholder="Password"></input>
+                                    <input type="text" class="form-control" name="twitter" placeholder="Password" onChange={e => onChange(e)}></input>
                                 </div>
-                            </div> }
-                            
+                            </div>)
+                            }
+
                         </div>
                         <div class="form-group">
                             <label >Bio</label>
-                            <textarea name="bio" class="form-control" rows="5" maxLength="200"></textarea>
+                            <textarea name="bio" class="form-control" rows="5" maxLength="200" onChange={e => onChange(e)}></textarea>
                         </div>
                         <div class="form-group">
                             <label >Skills</label>
-                            <input type="text" class="form-control" name="skill" placeholder="Password"></input>
-                        </div>
-                        <div class="form-group">
-                            <label >Experience</label>
-                            <input type="text" class="form-control" name="experience" placeholder="Password"></input>
+                            <input type="text" class="form-control" name="skills" placeholder="Password" onChange={e => onChange(e)}></input>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
-                
+
             </div>
-            
+
         </div>
-        
+
     )
 }
 
