3ca031f3266acdbe6ecdda370276a52d29f9c706
yashi12 <yashiagarwal1812@gmail.com>
2021-08-14 10:38:39 +0530

    Add(google Login)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,8 +2,11 @@ const {check, validationResult} =require('express-validator')
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const bcrypt = require('bcryptjs');
+const {OAuth2Client} = require('google-auth-library');
 
 const User =require('../models/User');
+
+const oAuth2Client = new OAuth2Client(process.env.CLIENT_ID);
 
 const getAuthUser = (req,res,next)=> {
     User.findById(req.user.id)
@@ -58,7 +61,72 @@ const validateUser = (req,res,next)=>{
         });
 }
 
+const googlelogin = (req,res,next)=>{
+    const {token} = req.body;
+    oAuth2Client.verifyIdToken({idToken:token,audience:process.env.CLIENT_ID})
+        .then(response=>{
+            const {email_verified,name,email} = response.getPayload();
+            if(email_verified){
+                User.findOne({email})
+                    .then(user =>{
+                        if(user){
+                            // user already exists in db
+                            const payload = {
+                                user: {
+                                    id: user._id
+                                }
+                            };
+                            jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: process.env.TOKEN_EXPIRE_TIME}, (err,token)=>{
+                                if (err){
+                                    throw err;
+                                    return res.status(500).send('server error...');
+                                }
+                                res.json({token});
+                            });
+                        }
+                        else{
+                            // create user in db
+                            let password = email+process.env.SECRET_KEY;
+                            bcrypt.hash(password, 12)
+                                .then(hashedPassword => {
+                                    const user = new User({
+                                        name, email, password: hashedPassword,confirmedEmail:email_verified
+                                    });
+                                    return user.save();
+                                })
+                                .then(user => {
+                                    const payload = {
+                                        user: {
+                                            id: user._id
+                                        }
+                                    };
+                                    jwt.sign(payload, config.get('SECRET_KEY'), {expiresIn: config.get('TOKEN_EXPIRE_TIME')}, (err,token)=>{
+                                        if (err){
+                                            throw err;
+                                            return res.status(500).send('server error...');
+                                        }
+                                        res.json({token});
+                                    });
+                                })
+                                .catch(err => {
+                                    console.log(err.message);
+                                    return res.status(500).send('Server error...');
+                                });
+                        }
+                    })
+                    .catch(err=>{
+                        console.log(err.message);
+                        return res.status(500).send('Server error...');
+                    })
+            }
+            else{
+                return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+            }
+        })
+}
+
 module.exports = {
     getAuthUser:getAuthUser,
-    validateUser:validateUser
+    validateUser:validateUser,
+    googlelogin:googlelogin
 }
