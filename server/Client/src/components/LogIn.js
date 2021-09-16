7b804ae6a6bc75e901f0833d287843183fa0283e
JayantGoel001 <jgoel92@gmail.com>
2021-09-16 20:12:14 +0530

    Upated Login register UI

--- server/Client/src/components/LogIn.js
+++ server/Client/src/components/LogIn.js
@@ -3,90 +3,89 @@ import PropTypes from 'prop-types'
 import { Link, Redirect } from 'react-router-dom'
 import { login } from '../action/auth'
 import  React,{ useState } from 'react'
-//import { GoogleLogin } from 'react-google-login';
+import './LogIn-Register.css';
 import axios from "axios";
 
 const LogIn = ({ login, isAuthenticated }) => {
 
-    const [formData, setData] = useState({
-        email : '',
-        password : ''
-    })
+	const [formData, setData] = useState({
+		email : '',
+		password : ''
+	})
 
-    const { email , password} = formData
+	const { email , password} = formData
 
-    const onChange = e => setData({...formData, [e.target.name]: e.target.value })
+	const onChange = e => setData({...formData, [e.target.name]: e.target.value })
 
-    const onSubmit = async e => {
-        e.preventDefault();
-        login({email, password})
-    }
+	const onSubmit = async e => {
+		e.preventDefault();
+		login({email, password})
+	}
 
-    // Redirect if logged in
+	// Redirect if logged in
 
-    if(isAuthenticated){
-        return <Redirect to='/profile' />
-    }
+	if(isAuthenticated){
+		return <Redirect to='/profile' />
+	}
 
-    const responseGoogleSuccess = (response) => {
-        console.log(response);
-        axios({
-            method:"POST",
-            url:"http://localhost:3000/api/auth/googlelogin",
-            data: {token: response.tokenId}
-        }).then(resp=>{
-            console.log("google login success",resp.data.token);
-            login({email, password})
-        })
-    }
+	const responseGoogleSuccess = (response) => {
+		console.log(response);
+		axios({
+			method:"POST",
+			url:"http://localhost:3000/api/auth/googlelogin",
+			data: {token: response.tokenId}
+		}).then(resp=>{
+			console.log("google login success",resp.data.token);
+			login({email, password})
+		})
+	}
 
-    const responseGoogle = (response) => {
-        console.log(response);
-    }
+	const responseGoogle = (response) => {
+		console.log(response);
+	}
+	return (
+		<div>
+			<div className="background">
+				<div className="shape"/>
+				<div className="shape"/>
+			</div>
+			<form className="bar" role="form" method="post" onSubmit={e => onSubmit(e)}>
+				<h3>Login Here</h3>
+				<label htmlFor="exampleInputEmail1">Email address</label>
+				<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required=""
+											   name="email" onChange={e => onChange(e)} autoComplete="off"/>
+				<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
 
-    return (
-        <div className="row">
-            <div className="col"/>
-            <div className="col">
-            <br/><br/>
-            <section className="search-bar">
-                <form className="bar" role="form" method="post" onSubmit={e => onSubmit(e)}>
-                    <div className="form-group">
-                        <h1>Log In</h1><br/>
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required=""
-    name="email" onChange={e => onChange(e)}/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input onChange={e => onChange(e)} type="password" className="form-control" id="exampleInputPassword1" name="password"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Login</button>
-                </form>
-                {/* <GoogleLogin
-                    clientId={process.env.REACT_APP_CID}
-                    buttonText="Login"
-                    onSuccess={responseGoogleSuccess}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                /> */}
-
-                </section>
-            </div>
-            <div className="col"/>
-
-        </div>
-    )
+				<label htmlFor="exampleInputPassword1">Password</label>
+				<input onChange={e => onChange(e)} type="password" className="form-control" id="exampleInputPassword1" name="password" />
+				<button type="submit" className="btn">Log In</button>
+				<div className="social">
+					<div className="go"
+						clientId={process.env.REACT_APP_CID}
+						buttonText="Login"
+						onSuccess={responseGoogleSuccess}
+						onFailure={responseGoogle}
+						cookiePolicy={'single_host_origin'}>
+						<i className="fab fa-google"/>
+						Google
+					</div>
+					<div className="fb">
+						<i className="fab fa-facebook"/>
+						Facebook
+					</div>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 LogIn.prototype = {
-    login: PropTypes.func.isRequired,
-    isAuthenticated: PropTypes.bool.isRequired
+	login: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool.isRequired
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+	isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps, {login})(LogIn)
