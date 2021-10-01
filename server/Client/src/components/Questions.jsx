8b3896edcd188f5a35e3510d68ec19e6a010d074
21Siddhantjain <2001siddhantjain@gmail.com>
2021-10-01 19:10:16 +0530

    display

--- server/Client/src/components/Questions.jsx
+++ server/Client/src/components/Questions.jsx
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {addLike, deletePost} from '../action/post'
 import post from "../reducers/post";
+import question from '../reducers/question';
 
-const Questions = ({addLike, deletePost, auth, post}, showActions) => {
+const Questions = ({ auth, question}) => {
+
+    console.log(question)
+
     return (
         <div>
             <div className="row">
@@ -15,12 +18,13 @@ const Questions = ({addLike, deletePost, auth, post}, showActions) => {
                 <div className="card mb-3 col-8">
                     <div className="row g-0">
                         <div className="col-md-4 mb-3">
+                            <br />
                             {post.image ?
-                                <img width="300" height="300"
+                                <img width="200" height="200"
                                      src={post.image}
                                     // src="https://media.wired.com/photos/5e59a85635982c0009f6eb8a/1:1/w_1350,h_1350,c_limit/python-popularity.jpg"
                                      alt="..."></img>
-                                : <img width="300" height="300"
+                                : <img width="200" height="200"
                                        // src={post.image}
                                     src="https://media.wired.com/photos/5e59a85635982c0009f6eb8a/1:1/w_1350,h_1350,c_limit/python-popularity.jpg"
                                        alt="..."></img>
@@ -29,42 +33,16 @@ const Questions = ({addLike, deletePost, auth, post}, showActions) => {
                         <div className="col-md-8">
                             <div className="row g-0">
                                 <div className="card-body">
-                                    <h5 className="card-title">{post.userName}</h5>
+                                    <h5 className="card-title">Q. {question.title}</h5>
                                     {/* <p className="card-text">{post.text}</p> */}
-                                    <div dangerouslySetInnerHTML={{__html: post.text}}></div>
+                                    <div dangerouslySetInnerHTML={{__html: question.description}}></div>
                                     <div>
-                                        {post.title.map((tag) => (
+                                        {question.tags.map((tag) => (
                                             <span className="badge badge-secondary">{tag}</span>
                                         ))}
                                     </div>
                                 </div>
                             </div>
-                            {showActions && (
-                                <div className="row g-1">
-                                    <div className="col-9"></div>
-                                    <div className="col-3">
-                                        <div className="card-body">
-                                            <button onClick={e => addLike(post._id)} type="button" className="btn btn-primary">
-                                            {/*<button onClick={e => addLikeNew(post._id)} type="button" className="btn btn-primary">*/}
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                     fill="currentColor" className="bi bi-hand-thumbs-up-fill"
-                                                     viewBox="0 0 16 16">
-                                                    <path
-                                                        d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-                                                </svg>
-                                                Like <span className="badge bg-secondary">{post.likeCount }</span>
-                                            </button>
-                                        </div>
-                                        {!auth.loading && post.user === auth.user._id && (
-                                            <div className="card-body">
-                                                <button onClick={() => deletePost(post._id)} type="button"
-                                                        className="btn btn-danger">Delete
-                                                </button>
-                                            </div>
-                                        )}
-
-                                    </div>
-                                </div>)}
                         </div>
                     </div>
                 </div>
@@ -78,15 +56,12 @@ Questions.defaultProps = {
 };
 
 Questions.propTypes = {
-    post: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired,
-    addLike: PropTypes.func.isRequired,
-    deletePost: PropTypes.func.isRequired,
-    showActions: PropTypes.bool
+    question: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps, {addLike, deletePost})(Questions)
+export default connect(mapStateToProps, {})(Questions)
--- server/Client/src/components/QuestionsFeed.jsx
+++ server/Client/src/components/QuestionsFeed.jsx
@@ -1,34 +1,41 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Posts from './Posts';
-import { getPosts } from '../action/post';
+import Questions from './Questions';
+import { getQuestions } from '../action/question';
+import Spinner from './Spinner';
 
-const QuestionsFeed = ({ getPosts, post: { posts } }) => {
+const QuestionsFeed = ({ getQuestions, question : {questions} }) => {
     useEffect(() => {
-        getPosts();
-        console.log("call get post");
-    }, [getPosts]);
+        getQuestions();
+    }, [getQuestions]);
 
     return (
-        <Fragment>
-            <h1 className="large text-primary">Questions</h1>
-            <div >
-                {posts.map((post) => (
-                    <Posts key={post._id} post={post} />
-                ))}
-            </div>
-        </Fragment>
+        <div>
+            {
+                questions ? <Fragment>
+                <h1 className="large text-primary">Questions</h1>
+                <div >
+                    {questions.map((question) => (
+                        <Questions key={question._id} question={question} />
+                    ))}
+                </div>
+                </Fragment> :
+                <Spinner />
+            }
+        </div>
+        
+        
     );
 };
 
-Posts.propTypes = {
-    getPosts: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired
+QuestionsFeed.propTypes = {
+    getQuestions: PropTypes.func.isRequired,
+    question: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-    post: state.post
+    question: state.question
 });
 
-export default connect(mapStateToProps, { getPosts })(QuestionsFeed);
+export default connect(mapStateToProps, { getQuestions })(QuestionsFeed);
