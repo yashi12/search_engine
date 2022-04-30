1e9b5c8692e0b77b24092193dcfbf4c988bc6e8d
21Siddhantjain <2001siddhantjain@gmail.com>
2022-04-30 15:31:17 +0530

    end session frontend integration

--- client/src/contracts/Doubt.sol
+++ client/src/contracts/Doubt.sol
@@ -60,7 +60,7 @@ contract Mycontract {
         return (_session.amount, _session.doubtAsker, _session.doubtSolver, _session.topic, _session.id, _session.isCompleted);
     }
 
-    function endSession(string calldata id) external payable {
+    function endSession(string calldata id) external {
         // Fetch the session
         Session memory _session = sessions[id];
         // Fetch address of doubt solver
