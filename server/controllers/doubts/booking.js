1934bc33c1e0325a5aa049c717026053ad0b030a
yashi12 <yashiagarwal1812@gmail.com>
2022-05-05 18:35:53 +0530

    update controller - change function acc to new model - add comment - remove unused code

--- server/controllers/doubts/booking.js
+++ server/controllers/doubts/booking.js
@@ -1,147 +1,150 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
-const {
-    check,
-    validationResult
-} = require('express-validator');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const request = require('request');
-const AWS = require('aws-sdk');
-const fetch = require('node-fetch');
-const Doubt = require('../../models/discussion/Doubt');
-const Answer = require('../../models/discussion/Answer');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const request = require("request");
+const AWS = require("aws-sdk");
+const fetch = require("node-fetch");
+const Doubt = require("../../models/discussion/Doubt");
+const Answer = require("../../models/discussion/Answer");
 // const Tag = require('../models/discussion/Tag');
-const Category = require('../../models/discussion/Category');
-const User = require('../../models/User');
-const Booking = require('../../models/discussion/Booking');
-const ContentMiddleware = require('../../middleware/content');
-const {
-    paginatedResults
-} = require('../helper/pagenation');
-const {raw} = require("config/raw");
+const Category = require("../../models/discussion/Category");
+const User = require("../../models/User");
+const Booking = require("../../models/discussion/Booking");
+const ContentMiddleware = require("../../middleware/content");
+const { paginatedResults } = require("../helper/pagenation");
+const { raw } = require("config/raw");
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET,
 });
 
-let FormData = require('form-data');
+let FormData = require("form-data");
+
+/* Here the user initiates a contract where his amount gets deducted but is not sent to the mentor
+the amount is held by the the third party application we are using
+*/
 
 const bookMentor = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(404).json({
-            errors: errors.array()
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({
+      errors: errors.array(),
+    });
+  }
+  try {
+    const doubtId = req.params.doubt_id;
+    const mentorDoubtId = req.params.id;
+    const userId = req.user.id;
+    const booking = await Booking.findOne({
+      doubtId: doubtId,
+      mentorId: mentorDoubtId,
+      userId: userId,
+    });
+    if (!booking) {
+      return res.status(404).json({
+        msg: "No such booking found",
+      });
+    } else {
+      Booking.findOneAndUpdate(
+        { doubtId: doubtId, mentorId: mentorDoubtId, userId: userId },
+        { $set: { status: "initiated" } },
+        { new: true }
+      )
+        .then((booking) => {
+          res.json(booking);
+        })
+        .catch((err) => {
+          res.status(500).send(err.message);
         });
     }
-    try {
-        const doubtId = req.params.doubt_id;
-        const mentorDoubtId = req.params.id;
-        const doubt = await Doubt.findOne(
-            {$and:[{_id:doubtId},
-                { mentor: { $elemMatch: {_id: mentorDoubtId } } }    
-            ]}
-        );
-        if (!doubt) {
-            return res.status(404).json({
-                msg: 'No such doubt found'
-            });
-        }else{
-            let tempObj={};
-            for(var i = 0; i < doubt.mentor.length; i++){
-                if(doubt.mentor[i]._id == mentorDoubtId){
-                    tempObj= {amount:doubt.mentor[i].amount,mentorId: doubt.mentor[i].mentorId};
-                }
-            }
-            const tempBooking = {};
-            tempBooking.doubtId = doubtId;
-            tempBooking.userId = req.user.id;
-            tempBooking.mentorId = tempObj.mentorId;
-            tempBooking.amount = tempObj.amount;
-            tempBooking.status = "initiated";
-            tempBooking.meetLink = "https://meet.google.com/frs-uzpg-fgv";
-            const newBooking = new Booking(tempBooking);
-                newBooking.save()
-                    .then(booking => {
-                        res.json(booking);
-                    })
-                    .catch(err => {
-                        res.status(500).send(err.message);
-                    })
-        }
-
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server Error...');
-    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error...");
+  }
 };
+
+/* Mentor confirms a doubt and finally the contact is legal ie. signed by both parties
+ */
 
 const confirmMentor = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(404).json({
-            errors: errors.array()
-        });
-    }
-    try {
-        const bookingId = req.params.id;
-        const userId = req.user.id;
-        Booking.findOneAndUpdate(
-            {$and:[{_id:bookingId},
-                { userId: userId }    
-            ]},
-            {$set: {status:"accepted"}},
-            {new:true}
-        ) .then(booking => {
-            console.log(booking);
-            res.json(booking);
-        })
-        .catch(err => {
-            res.status(500).send(err.message);
-        })
-
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server Error...');
-    }
+  console.log("confirm mentor");
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({
+      errors: errors.array(),
+    });
+  }
+  try {
+    const bookingId = req.params.id;
+    const userId = req.user.id;
+    Booking.findOneAndUpdate(
+      { $and: [{ _id: bookingId }, { mentorId: userId }] },
+      { $set: { status: "accepted" } },
+      { new: true }
+    )
+      .then((booking) => {
+        if (booking == null)
+          res.status(404).send("no such booking is initiated by the user");
+        else {
+          res.json(booking);
+          Doubt.findOneAndUpdate(
+            { _id: booking.doubtId },
+            { $set: { status: "in process" } },
+            { new: true }
+          ).then((doubt) => {});
+        }
+      })
+      .catch((err) => {
+        res.status(500).send(err.message);
+      });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error...");
+  }
 };
+
+/* Mark doubt finally as solved, many conditions
+- user marks as solved (currently fulfilling)
+- mentor says solved, user denies
+- mentor denies
+*/
 
 const bookedDoubtSolved = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(404).json({
-            errors: errors.array()
-        });
-    }
-    try {
-        const bookingId = req.params.id;
-        const userId = req.user.id;
-        Booking.findOneAndUpdate(
-            {$and:[{_id:bookingId},
-                { userId: userId }    
-            ]},
-            {$set: {status:"solved"}},
-            {new:true}
-        ) .then(booking => {
-            console.log(booking);
-            res.json(booking);
-        })
-        .catch(err => {
-            res.status(500).send(err.message);
-        })
-
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server Error...');
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(404).json({
+      errors: errors.array(),
+    });
+  }
+  try {
+    const bookingId = req.params.id;
+    const userId = req.user.id;
+    Booking.findOneAndUpdate(
+      { $and: [{ _id: bookingId }, { userId: userId }] },
+      { $set: { status: "solved" } },
+      { new: true }
+    )
+      .then((booking) => {
+        if (booking == null) res.status(404).send("no such booking found");
+        else {
+          res.json(booking);
+        }
+      })
+      .catch((err) => {
+        res.status(500).send(err.message);
+      });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error...");
+  }
 };
 
-
-
 module.exports = {
-    bookMentor: bookMentor,
-    confirmMentor: confirmMentor,
-    bookedDoubtSolved: bookedDoubtSolved
-}
+  bookMentor: bookMentor,
+  confirmMentor: confirmMentor,
+  bookedDoubtSolved: bookedDoubtSolved,
+};
